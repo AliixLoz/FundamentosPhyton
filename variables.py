6d07b219d11b561099e7404930c19ecc006a5530
Aliix <loveitza50@gmail.com>
2023-06-09 13:24:16 -0600

    02 Variables

---
+++ variables.py
@@ -0,0 +1,13 @@
+# Python es no Tipado 
+
+name= "Aliix"
+age= 18
+height= 1.54
+is_student= True
+children= []
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+print(type(children))
